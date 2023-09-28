1bd6a68df4b1e68ea0f67efc64bc0a7729ff01dd
Mihai Simion <mihai@simion.dev>
2023-09-28 11:34:07 +0300

    Implemented a feature that allows users to analyze the sentiment of the text content scraped from the webpage

--- src/app.py
+++ src/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 from bs4 import BeautifulSoup
+from textblob import TextBlob
 import requests
 
 app = Flask(__name__)
@@ -22,11 +23,31 @@ def scrape():
         if response.status_code == 200:
             soup = BeautifulSoup(response.text, 'html.parser')
 
-            result = {}
+            scraped_data = {}
             for element in elements:
-                result[element] = [tag.text for tag in soup.find_all(element)]            
+                if element == 'img':
+                    scraped_data[element] = [e.get('alt') for e in soup.find_all('img')]
+                else:
+                    scraped_data[element] = [e.text for e in soup.find_all(element)]
 
-            return jsonify(result), 200
+            # Perform sentiment analysis
+            for element, text_list in scraped_data.items():
+                sentiment_scores = []
+                for text in text_list:
+                    analysis = TextBlob(text)
+                    sentiment_scores.append(analysis.sentiment.polarity)
+
+                # Determine overall sentiment
+                if len(sentiment_scores) == 0:
+                    overall_sentiment = 0
+                else:
+                    overall_sentiment = sum(sentiment_scores) / len(sentiment_scores)
+                    scraped_data[element] = {
+                        'text': text_list,
+                        'overall_sentiment': 'positive' if overall_sentiment > 0 else 'negative' if overall_sentiment < 0 else 'neutral',
+                        'sentiment_scores': sentiment_scores
+                }
+            return jsonify(scraped_data), 200
         else:
             return jsonify({'error': 'Failed to fetch the URL'}), 500
     except Exception as e:
