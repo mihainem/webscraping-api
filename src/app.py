21ef16c9cab12e259243e4ab855339fbaa062874
Mihai Simion <mihai@simion.dev>
2023-09-28 12:57:48 +0300

    build a basic front-end with Flask (never used Flask before)

--- src/app.py
+++ src/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, render_template, jsonify
 from bs4 import BeautifulSoup
 from textblob import TextBlob
 import requests
@@ -7,60 +7,66 @@ import re
 app = Flask(__name__)
 
 
-@app.route('/scrape', methods=['POST'])
-def scrape():
-    try:
-        #Get the URL from the request body
-        data= request.get_json()
-        url = data['url']
+@app.route('/', methods=['POST', 'GET'])
+def index():
+    if request.method == 'POST':
+        try:
+            #Get the URL from the request body
+            url = request.form.get('url')
 
-        #TODO: make sure the user knows to separate elements by commas
-        elements_string = data['elements-to-scrape']
-        elements = [element.strip() for element in elements_string.split(',')]
+            if not url:
+                return render_template('index.html', data={'error': 'URL is required'})
 
-        #Make a request to the URL we want to scrape
-        response = requests.get(url)
+            #TODO: make sure the user knows to separate elements by commas
+            elements_string = request.form.get('elements-to-scrape') or "h3, p, span"
+            elements = [element.strip() for element in elements_string.split(',')]
 
-        if response.status_code == 200:
-            soup = BeautifulSoup(response.text, 'html.parser')
+            #Make a request to the URL we want to scrape
+            response = requests.get(url)
 
-            scraped_data = {}
-            for element in elements:
-                if element == 'img':
-                    scraped_data[element] = [e.get('alt') for e in soup.find_all('img')]
-                else:
-                    scraped_data[element] = [e.text for e in soup.find_all(element)]
+            if response.status_code == 200:
+                soup = BeautifulSoup(response.text, 'html.parser')
 
-            # Perform sentiment analysis
-            for element, text_list in scraped_data.items():
-                sentiment_scores = []
-                total_words = 0
-                for text in text_list:
-                    analysis = TextBlob(text)
-                    sentiment_scores.append(analysis.sentiment.polarity)
+                scraped_data = {}
+                for element in elements:
+                    if element == 'img':
+                        scraped_data[element] = [e.get('alt') for e in soup.find_all('img')]
+                    else:
+                        scraped_data[element] = [e.text for e in soup.find_all(element)]
 
-                    # Determine word-count
-                    words = re.findall(r'\w+', text)  # Use regex to find words
-                    total_words += len(words)
+                # Perform sentiment analysis
+                for element, text_list in scraped_data.items():
+                    sentiment_scores = []
+                    total_words = 0
+                    for text in text_list:
+                        analysis = TextBlob(text)
+                        sentiment_scores.append(analysis.sentiment.polarity)
 
-                # Determine overall sentiment
-                if len(sentiment_scores) == 0:
-                    overall_sentiment = 0
-                else:
-                    overall_sentiment = sum(sentiment_scores) / len(sentiment_scores)
-                    scraped_data[element] = {
-                        'text': text_list,
-                        'word-count': total_words,
-                        'overall_sentiment': 'positive' if overall_sentiment > 0 else 'negative' if overall_sentiment < 0 else 'neutral',
-                        'sentiment_scores': sentiment_scores
-                }
-                    
-       
-            return jsonify(scraped_data), 200
-        else:
-            return jsonify({'error': 'Failed to fetch the URL'}), 500
-    except Exception as e:
-        return jsonify({'error': str(e)}), 400
+                        # Determine word-count
+                        words = re.findall(r'\w+', text)  # Use regex to find words
+                        total_words += len(words)
+
+                    # Determine overall sentiment
+                    if len(sentiment_scores) == 0:
+                        overall_sentiment = 0
+                    else:
+                        overall_sentiment = sum(sentiment_scores) / len(sentiment_scores)
+                        scraped_data[element] = {
+                            'text': text_list,
+                            'word-count': total_words,
+                            'overall_sentiment': 'positive' if overall_sentiment > 0 else 'negative' if overall_sentiment < 0 else 'neutral',
+                            'sentiment_scores': sentiment_scores
+                    }      
+        
+                return render_template('index.html', data=scraped_data)
+            else:
+                return render_template('index.html', data={'error': 'Failed to fetch the URL'})
+        except Exception as e:
+            return render_template('index.html', data={'error': str(e)})
+    
+    # For GET requests, render the initial form
+    return render_template('index.html', data=None)
+
     
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0", port=8080)
